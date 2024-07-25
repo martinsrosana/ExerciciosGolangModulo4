84be754a63cf4bf93627ea2316bf9748c086d167
rosamartins_pessoal <rosana.sbo@gmail.com>
2024-07-25 15:27:57 -0300

    Testes Avançados 2

--- Testes Avancados/formas/formas.go
+++ Testes Avancados/formas/formas.go
@@ -1,1 +1,41 @@
 package formas
+
+import (
+	"math"
+)
+
+// Forma é uma interface que define o comportamento de uma forma geométrica
+type Forma interface {
+	area() float64
+}
+
+// Retangulo é uma struct que representa um retângulo
+type Retangulo struct {
+	Altura  float64
+	Largura float64
+}
+
+// Area calcula a área do retângulo
+func (r Retangulo) Area() float64 {
+	return r.Altura * r.Largura
+}
+
+// Circulo é uma struct que representa um círculo
+type Circulo struct {
+	Raio float64
+}
+
+// Area calcula a área do círculo
+func (c Circulo) Area() float64 {
+	return math.Pi * (c.Raio * c.Raio) //ou return math.Pi * math.Pow (c.raio, 2)
+}
+
+// Quadrado é uma struct que representa um quadrado
+type Quadrado struct {
+	Lado float64
+}
+
+// Area calcula a área do quadrado
+func (q Quadrado) Area() float64 {
+	return math.Pow(q.Lado, 2)
+}
--- Testes Avancados/formas/formas_test.go
+++ Testes Avancados/formas/formas_test.go
@@ -1,1 +1,31 @@
 package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	//TDD - Test Driven Development - Desenvolvimento Orientado a Testes - Red, Green, Refactor (Criar o teste, Fazer o teste passar, Refatorar o código)
+
+	t.Run("Retangulo", func(t *testing.T) {
+		retangulo := Retangulo{10, 12}
+
+		areaEsperada := float64(120)
+		areaRecebida := retangulo.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Errorf("Area esperada %f, mas recebida %f", areaEsperada, areaRecebida)
+		}
+	})
+	t.Run("Circulo", func(t *testing.T) {
+		circulo := Circulo{10}
+
+		areaEsperada := float64(math.Pi * 100)
+		areaRecebida := circulo.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Errorf("Area esperada %f, mas recebida %f", areaEsperada, areaRecebida)
+		}
+	})
+}
